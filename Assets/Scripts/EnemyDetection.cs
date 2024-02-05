0c73b8b107ff262646e23562b94b72e09655e862
Josh Frost <jwfrost2002@gmail.com>
2024-02-05 19:35:35 +0000

    Add simple trigger collider detection

---
+++ Assets/Scripts/EnemyDetection.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetection : MonoBehaviour
+{
+    private float timer = 0f;
+    private GameObject thisEnemy;
+
+    void Awake() 
+    {
+        thisEnemy = this.transform.parent.gameObject;
+        Debug.Log(thisEnemy.name);
+    }
+
+    void OnTriggerStay(Collider other) 
+    {
+        if (other.name == "Player") {
+            timer += Time.deltaTime;
+
+            if (timer >= 0.5f) {
+                Debug.Log("Player spotted");
+                timer = 0f;
+            }
+        }
+        else Debug.Log(other.name);
+    }
+}
